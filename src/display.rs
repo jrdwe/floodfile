6f51d196b0a87bdbfd31cfd6aa01909299864110
jrdwe <jarryd.webb@hotmail.com>
2024-12-31 09:21:57 +1100

    feat(interface): filelist buttons

--- src/display.rs
+++ src/display.rs
@@ -1,24 +1,56 @@
-use crate::network::usable_interfaces;
+use crate::network::{usable_interfaces, Channel};
 use crossbeam::channel::{unbounded, Receiver, Sender};
 use cursive::{
     traits::{Nameable, Resizable, Scrollable},
     view::ScrollStrategy,
-    views::{Dialog, EditView, LinearLayout, Panel, SelectView, TextView},
+    views::{Button, Dialog, EditView, LinearLayout, Panel, SelectView, TextView},
 };
+use pnet::datalink::NetworkInterface;
 use std::thread;
 
 enum DisplayCommand {
     NewFile(String),
     FetchFile(String),
-    ChangeInterface(String),
 }
 
 enum NetworkCommand {
     AdvertiseFile(String),
     SendFile(String),
+    ChangeInterface(String),
 }
 
 fn network_thread(display_tx: Sender<DisplayCommand>, network_rx: Receiver<NetworkCommand>) {
+    let interfaces = usable_interfaces();
+    let mut channel = Channel::new(interfaces[0].clone());
+
+    loop {
+        while let Ok(command) = network_rx.try_recv() {
+            match command {
+                NetworkCommand::AdvertiseFile(filepath) => {
+                    println!("filepath: {}", filepath);
+                }
+                NetworkCommand::SendFile(id) => {
+                    println!("id: {}", id);
+                    // grab from disk
+                    // spam over network!
+                }
+                NetworkCommand::ChangeInterface(name) => {
+                    if channel.interface_name() == name {
+                        continue;
+                    }
+
+                    let interface = usable_interfaces()
+                        .into_iter()
+                        .find(|i| i.name == name)
+                        .unwrap();
+
+                    channel = Channel::new(interface);
+                }
+            }
+        }
+    }
+
+    // needs to set a channel -> define in networking
     // send display updates and receive network updates
 }
 
@@ -26,7 +58,11 @@ pub fn run() {
     let (display_tx, display_rx) = unbounded::<DisplayCommand>();
     let (network_tx, network_rx) = unbounded::<NetworkCommand>();
 
-    thread::spawn(move || network_thread(display_tx.clone(), network_rx));
+    thread::spawn({
+        // requires that move takes no ownership of display_tx
+        let display_tx = display_tx.clone();
+        move || network_thread(display_tx, network_rx)
+    });
 
     let mut siv = cursive::default();
     siv.set_autohide_menu(false);
@@ -44,9 +80,13 @@ pub fn run() {
                 LinearLayout::vertical()
                     .child(
                         Dialog::around(
-                            Panel::new(EditView::new())
-                                .title("file path")
-                                .with_name("file_input"),
+                            Panel::new(EditView::new().on_submit(move |_siv, name: &str| {
+                                display_tx
+                                    .send(DisplayCommand::NewFile(name.to_string()))
+                                    .unwrap()
+                            }))
+                            .title("file path")
+                            .with_name("file_input"),
                         )
                         .title("send a file over arp")
                         .full_width(),
@@ -59,7 +99,7 @@ pub fn run() {
                                         (format!("{0}: {1}", i.name, i.mac.unwrap()), i.name)
                                     }),
                                 )
-                                .on_submit(|siv, name: &String| ()),
+                                .on_submit(move |_siv, name: &String| ()),
                         )
                         .title("interfaces")
                         .full_height(),
@@ -78,5 +118,27 @@ pub fn run() {
             ),
     );
 
-    siv.run();
+    let mut siv = siv.runner();
+    while siv.is_running() {
+        siv.refresh();
+        while let Ok(command) = display_rx.try_recv() {
+            match command {
+                DisplayCommand::NewFile(file) => {
+                    siv.call_on_name("file_list", |file_list: &mut LinearLayout| {
+                        let available =
+                            Dialog::around(TextView::new(file)).button("download", |s| s.quit());
+
+                        file_list.add_child(available);
+                    });
+                }
+                DisplayCommand::FetchFile(id) => {
+                    println!("id: {}", id);
+                }
+            }
+        }
+
+        siv.step();
+    }
+
+    // loop over ui command here!
 }
--- src/network.rs
+++ src/network.rs
@@ -1,4 +1,8 @@
-use pnet::datalink::NetworkInterface;
+use pnet::datalink::Channel::Ethernet;
+use pnet::{
+    datalink::{DataLinkReceiver, DataLinkSender, NetworkInterface},
+    util::MacAddr,
+};
 
 pub fn usable_interfaces() -> Vec<NetworkInterface> {
     let mut interfaces = pnet::datalink::interfaces()
@@ -6,7 +10,39 @@ pub fn usable_interfaces() -> Vec<NetworkInterface> {
         .filter(|i| i.mac.is_some() && !i.ips.is_empty())
         .collect::<Vec<NetworkInterface>>();
 
-    interfaces.sort_unstable_by_key(|i| i.ips.len());
+    interfaces.sort_by_key(|i| i.ips.len());
     interfaces.reverse();
+
     interfaces
+}
+
+pub struct Channel {
+    src_mac_addr: MacAddr,
+    interface: NetworkInterface,
+    tx: Box<dyn DataLinkSender>,
+    rx: Box<dyn DataLinkReceiver>,
+}
+
+impl Channel {
+    pub fn new(interface: NetworkInterface) -> Self {
+        let (tx, rx) = match pnet::datalink::channel(&interface, Default::default()) {
+            Ok(Ethernet(tx, rx)) => (tx, rx),
+            Ok(_) => panic!("Unhandled channel type"),
+            Err(e) => panic!("Error occurred fetching channel: {}", e),
+        };
+
+        Self {
+            src_mac_addr: interface.mac.unwrap(),
+            interface,
+            tx,
+            rx,
+        }
+    }
+
+    pub fn interface_name(&self) -> String {
+        self.interface.name.clone()
+    }
+
+    // send to network func
+    // fetch from network func
 }
